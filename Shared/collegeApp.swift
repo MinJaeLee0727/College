c29e80ef251d7cf511240ecc052f03daee2393b8
# Chris Lee <minjaelee0727@gmail.com>
2021-02-20 19:56:53 +0900

    Initial Commit

---
+++ Shared/collegeApp.swift
@@ -0,0 +1,17 @@
+//
+//  collegeApp.swift
+//  Shared
+//
+//  Created by Min Jae Lee on 2021/02/20.
+//
+
+import SwiftUI
+
+@main
+struct collegeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
